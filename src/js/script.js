a7c2ca5f0f0ae6eb134aa5963926d545815085a8
henriquebottidev <henriquembotti@gmail.com>
2024-05-02 15:26:14 -0300

    first js file added

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 
 
+let imagens = ['src/assets/img/bike-laranja.png', 'src/assets/img/bike-vermelha.png']; 
+let index = 0; 
+let time = 3000; 
+
+function slideShow () {
+    document.getElementById("product-image").src=imagens[index]; 
+    index++; 
+
+    if (index == imagens.length) {
+        index = 0; 
+    }
+
+    setTimeout('slideshow()', time); 
+}
+slideShow(); 
