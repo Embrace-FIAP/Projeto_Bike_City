e795cac832b060c92df04fedfdd38817fdc68e4d
henriquebottidev <henriquembotti@gmail.com>
2024-05-04 10:06:45 -0300

    Slides chganges with click on button added

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,10 @@
 
 
-let imagens = ['src/assets/img/bike-laranja.png', 'src/assets/img/bike-vermelha.png']; 
-let index = 0; 
-let time = 3000; 
+let images = ['src/assets/img/bike-laranja.png', 'src/assets/img/bike-bege.png', 'src/assets/img/bike-branca.png']; 
+let imageInd = 0; 
+let intervalId = null; 
 
+/* 
 function slideShow () {
     document.getElementById("product-image").src=imagens[index]; 
     index++; 
@@ -12,6 +13,45 @@ function slideShow () {
         index = 0; 
     }
 
-    setTimeout('slideshow()', time); 
-}
+    setTimeout(slideShow, time); 
+} 
 slideShow(); 
+*/ 
+
+
+function nextSlide() {
+    imageInd++; 
+    if(imageInd >= images.length) {
+        imageInd = 0; 
+    }
+    updateImageDisplay(); 
+}
+
+function prevSlide(){
+    imageInd--; 
+    if(imageInd < 0) {
+        imageInd = images.length - 1
+    }
+    updateImageDisplay(); 
+}
+
+function updateImageDisplay(){
+    const imageElement = document.getElementById("product-image"); 
+    if(imageElement) {
+        imageElement.src=images[imageInd]
+    }
+}
+
+function initializeImage(){
+    updateImageDisplay(); 
+}
+
+document.addEventListener("DOMContentLoaded", initializeImage); 
+
+function changeColor(){
+    let textColor = document.getElementById("product-title"); 
+    if(textColor){
+        
+    }
+
+}
